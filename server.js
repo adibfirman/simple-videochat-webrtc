9c86b850ac07a744bce7588ee26087ac4cc9ceec
adibfirman <dib.firman@gmail.com>
2020-03-22 20:02:24 +0700

    fix port on server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 let clients = 0;
 
